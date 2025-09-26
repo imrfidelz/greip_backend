e5bdefa3bcd519172775493a67f8a5aa5e8faf7b
imrfidelz <imrfidelz@gmail.com>
2025-09-26 11:38:25 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,19 @@
 
 const express = require('express');
 const {
+  getUserByESN
+} = require('../controllers/users');
+const {
   createWaitlist
 } = require('../controllers/waitlist');
 const User = require('../models/User');
+
 const router = express.Router({ mergeParams: true });
 const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
 // Public route
 router.post('/waitlist', createWaitlist);
+router.get('/esn/:esn', getUserByESN);
 
 module.exports = router;
